9fb565a94bca1ddae72d241338c2ff6e4e74283f
Joel Wakefield <joeltwakefield@gmail.com>
2023-04-23 20:31:38 -0600

    explotion!

--- ship.gd
+++ ship.gd
@@ -4,7 +4,7 @@ extends Node3D
 @export var itemCount:float = 10.0
 
 var items:Array[RigidBody3D] = []
-
+var firstPass:bool = true
 
 func _ready():
 	var deb = preload("res://debris.tscn")
@@ -13,22 +13,37 @@ func _ready():
 		var item = deb.instantiate()
 		add_child(item)
 		items.append(item)
-		
-		item.apply_impulse(
-			Vector3(
-				randf_range(-28,28),
-				randf_range(-28,28),
-				randf_range(-28,28)
-			)
+	
+	for i in items.size():
+		items[i].transform.origin = Vector3(
+			i - 5,
+			(i % 3) - 1,
+			((i+1) % 3) - 1,
 		)
-		item.apply_torque(
+		
+		items[i].apply_impulse(
+			global_position.normalized() * randf_range(20,200)
+		)
+		items[i].apply_torque(
 			Vector3(
-				randf_range(-9,9),
-				randf_range(-9,9),
-				randf_range(-9,9)
+				randf_range(-90,90),
+				randf_range(-90,90),
+				randf_range(-90,90)
 			)
 		)
 
+
 func _physics_process(_delta):
-	for i in items.size():
-		print(items[i].transform.origin)
+	if firstPass:
+		firstPass = false
+		for i in items.size():
+			items[i].apply_impulse(
+				items[i].transform.origin.normalized() * randf_range(4,40)
+			)
+			items[i].apply_torque(
+				Vector3(
+					randf_range(-40,40),
+					randf_range(-40,40),
+					randf_range(-40,40)
+				)
+			)
